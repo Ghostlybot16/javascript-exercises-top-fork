d135fb55948dbb5479c88bf7f6b21677eb6034f1
Kramptj KC <81461556+Ghostlybot16@users.noreply.github.com>
2024-06-04 14:19:18 -0400

    Finished 03_revereString task
    Task was to reverse a string. It was accomplished
    via the use of a for loop. A second solution was also
    provided which used the built in string methods to
    split, reverse and join. All tests passes within
    the reverseString.spec.js file.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,25 @@
-const reverseString = function() {
+const reverseString = function(text) {
+
+    // A variable to store new string
+    let reversedStr = "";
+
+    // text.length - 1 represents the last object present within the text var string
+    // for loop starts at the last letter and iterates backwards.
+    for(let i = text.length - 1; i >= 0; i--){
+        
+        
+        reversedStr += text[i];   // <-- Same As -->  reversedStr = reversedStr + text[i]
+    }
+
+    return reversedStr;
+
+
+
+
+    // Another Solution
+
+    // splits the text at blankspaces, reverses the string and joins it back.
+    // return text.split("").reverse().join("")
 
 };
 
