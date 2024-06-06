1bba037a456a08662d28f37495f2db72dc30ea28
Kramptj KC <81461556+Ghostlybot16@users.noreply.github.com>
2024-06-06 17:42:45 -0400

    Finished 04_removeFromArray
    This task was to work with variadic functions where
    the function has two input parameters where one
    is an array and the second one is a rest parameter.
    The goal of the task to output an array that only
    had values which were not common between the two
    arrays. The task was done successfully and the
    desired output was met. All tests were successfull.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...num) {
+
+    // Sorting Arrays
+    sorted_array = array.toSorted(); //first array
+    sorted_num = num.toSorted(); // array containing elements that need to be discarded
+
+    const nonCommonElements = [];
+
+    // Iterate through sorted_array  
+    for(let i = 0; i < sorted_array.length; i++){
+
+        // Add the element to nonCommonElements if the sorted_array values are not in sorted_num array
+        if(!sorted_num.includes(sorted_array[i])){
+            nonCommonElements.push(sorted_array[i])
+        }
+    }
+    
+    return nonCommonElements;
+
 };
 
 // Do not edit below this line
