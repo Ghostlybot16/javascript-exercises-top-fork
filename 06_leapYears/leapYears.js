da976a4a5ff425688bc46d42f878a527072324d0
Kramptj KC <81461556+Ghostlybot16@users.noreply.github.com>
2024-06-07 17:55:26 -0400

    Finished 06_leapYears
    The task was to identify leap years regardless of
    past, present or future years. Modulus checks were
    required to check for divisibility in order to
    indentify leap years. All tests within leapYears.spec.js
    had passed successfully.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,24 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+// year is divisible by 100 but not by 400 (isNotLeapYear)
+if(year % 100 === 0 && year % 400 !== 0){
+    return false;
+}
+
+// year is divisible by 100 and 400 (isLeapYear)
+else if(year % 100 === 0 && year % 400 === 0){
+    return true;
+}
+
+// year is divisible by 4 (isLeapYear)
+else if(year % 4 === 0 ){
+    return true;
+}
+
+else{
+    return false;
+}
+
 
 };
 
