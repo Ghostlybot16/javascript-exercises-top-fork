f1b598aca110fe101123c9850b9cc8160e6a47dd
Kramptj KC <81461556+Ghostlybot16@users.noreply.github.com>
2024-06-08 15:22:02 -0400

    Finished 07_tempConversion
    The task was to convert between Celsius and Fahrenheit.
    The formula was implemented properly. The challenge
    of this task was to figure out a different way of
    rounding values rather than using the .ToFixed() function.
    All test cases had passed and proper output was acheived.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f_val) {
+
+  // Formula to convert to Celsius from Fahrenheit
+  let convertedVal = ((f_val) - 32) * (5/9);
+
+  const roundedVal = Math.round(convertedVal * 10)/10; // rounding to one decimal place
+
+  return roundedVal;
+  
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c_val) {
+  
+  // Formula to convert to Fahrenheit from Celsius
+  convertedVal = (c_val * (9/5)) + 32;
+
+  const roundedVal = Math.round(convertedVal * 10)/10; // rounding to one decimal place
+
+  return roundedVal;
 };
 
 // Do not edit below this line
